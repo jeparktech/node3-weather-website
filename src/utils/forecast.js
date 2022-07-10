64d9751a4e6fb681192aaa2edada06e6c2280c36
Jeongwon Park <jepark044@gmail.com>
2022-07-10 15:30:29 -0400

    updates the forcast information to show

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (longitude, latitude, callback) => {
             callback('Unable to find the location!', undefined)
         } else {
             const current = body.current
-            callback(undefined, `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`)
+            callback(undefined, `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out. The humidity is ${current.humidity}%.`)
         }
     })
 }
